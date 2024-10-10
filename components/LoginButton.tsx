faa19e2d6e7796c1b1c9d8d949feabaed8995fbb
marc condon <marc@fogtest.com>
2024-10-10 15:05:46 -0700

    restarting with project template from [Supabase DOCS Auth Page](https://supabase.com/docs/guides/auth/quickstarts/nextjs) because RLS did not work with other authentication scheme

---
+++ components/LoginButton.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { loginAction } from "@/actions/users";
+import { Provider } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { GitHubIcon } from "./ui/social-icons";
+
+function LoginButton() {
+  const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+
+  const handleClickLoginButton = (provider: Provider) => {
+    startTransition(async () => {
+      const { errorMessage, url } = await loginAction(provider);
+      if (!errorMessage && url) {
+        console.log('redirecting to ' + url)
+        router.push(url);
+      } else {
+        toast.error(errorMessage);
+      }
+    });
+  };
+
+  return (
+    <button
+      className="bg-black border-white border w-48 py-2 rounded-md hover:bg-emerald-950 flex items-center justify-center gap-2"
+      onClick={() => handleClickLoginButton("github")}
+      disabled={isPending}
+    >
+      <GitHubIcon />
+      {isPending ? "Logging in..." : "Login with GitHub"}
+    </button>
+  );
+}
+
+export default LoginButton;
