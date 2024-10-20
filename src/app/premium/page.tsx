535cefb1fc28846435e4c63c499e77bd9c2bf50e
marc condon <marc@fogtest.com>
2024-10-20 14:14:12 -0700

    more debug logs to see if it's server-kinde or mongo

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -5,9 +5,11 @@ import { redirect } from "next/navigation";
 const Page = async () => {
 	const { getUser } = getKindeServerSession();
 	const user = await getUser();
+	console.log('Premium kinde user ' + JSON.stringify(user));
 	if (!user) return redirect("/");
 
 	const userProfile = await prisma.user.findUnique({ where: { id: user.id } });
+	console.log('Premium mongo userProfile ' + JSON.stringify(userProfile));
 	if (userProfile?.plan === "free") return redirect("/");
 
 	return <div className='max-w-7xl mx-auto'>You are on the premium plan so you can see this page</div>;
